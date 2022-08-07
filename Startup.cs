2eac650bc13aaedaed77b830c1e4e9690d0e3fef
Roman Matanin <romashka951@gmail.com>
2022-08-07 19:01:29 +0300

    Использование функциональных средств платформы, добавлены стили, конец главы 15.  427

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using Platform.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
 
 namespace Platform
 {
@@ -22,7 +17,7 @@ namespace Platform
             Configuration = configuration;
         }
 
-        
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -31,39 +26,32 @@ namespace Platform
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseRouting();
-
-            app.UseMiddleware<LocationMiddleware>();
-
-            app.Use(async (context, next) =>
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
             {
-                string defaultDebug = Configuration["Logging:LogLevel:Default"];
-                await context.Response.WriteAsync($"The config setting is: {defaultDebug}");
-                string environ = Configuration["ASPNETCORE_ENVIRONMENT"];
-                await context.Response.WriteAsync($"\nTHE ENV SETTING IS: {environ}");
-                string wsID = Configuration["WebService:Id"];
-                string wsKey = Configuration["WebService:Key"];
-                await context.Response.WriteAsync($"\nTHE secret id IS: {wsID}");
-                await context.Response.WriteAsync($"\nTHE secret Key IS: {wsKey}");
+                FileProvider = new PhysicalFileProvider($"{env.ContentRootPath}/staticfiles"),
+                RequestPath = "/files"
             });
+            app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.Map("/", async context =>
                 {
-                    await context.Response.WriteAsync("\nHello world!");
+                    logger.LogDebug("Response for / started");
+                    await context.Response.WriteAsync("Hello world!");
+                    logger.LogDebug("Response for / completd");
                 });
             });
 
-            
+
 
         }
     }
