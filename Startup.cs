cfbb2c02ed33a59e81d7e73a7e83fbe0b104325a
Roman Matanin <romashka951@gmail.com>
2022-08-19 14:14:48 +0300

    Добавление сеансов в проект 442

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,14 @@ namespace Platform
             {
                 opts.CheckConsentNeeded = context => true;
             });
+
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,27 +35,41 @@ namespace Platform
 
             app.UseMiddleware<ConsentMiddleware>();
 
+            app.UseSession();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.Map("/cookie", async context =>
                 {
-                    int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0")+1;
-                    context.Response.Cookies.Append("counter1", counter1.ToString(), new CookieOptions { MaxAge = TimeSpan.FromMinutes(30), IsEssential = true }) ;
+                    /// Использование сеансов
+                    int counter1 = (context.Session.GetInt32("counter1") ?? 0) + 1;
+                    int counter2 = (context.Session.GetInt32("counter2") ?? 0) + 1;
 
-                    int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
-                    context.Response.Cookies.Append("counter2", counter2.ToString(), new CookieOptions { MaxAge = TimeSpan.FromMinutes(30) });
+                    context.Session.SetInt32("counter1", counter1);
+                    context.Session.SetInt32("counter2", counter2);
 
+                    await context.Session.CommitAsync();
                     await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
+
+                    /// Использование ckookie
+                    //int counter1 = int.Parse(context.Request.Cookies["counter1"] ?? "0")+1;
+                    //context.Response.Cookies.Append("counter1", counter1.ToString(), new CookieOptions { MaxAge = TimeSpan.FromMinutes(30), IsEssential = true }) ;
+
+                    //int counter2 = int.Parse(context.Request.Cookies["counter2"] ?? "0") + 1;
+                    //context.Response.Cookies.Append("counter2", counter2.ToString(), new CookieOptions { MaxAge = TimeSpan.FromMinutes(30) });
+
+                    //await context.Response.WriteAsync($"Counter1: {counter1}, Counter2: {counter2}");
                 });
-                endpoints.MapGet("clear", context =>
-                {
-                    context.Response.Cookies.Delete("counter1");
-                    context.Response.Cookies.Delete("counter2");
-                    context.Response.Redirect("/");
-                    return Task.CompletedTask;
-                });
+                /// Отчистка ckookie
+                //endpoints.MapGet("clear", context =>
+                //{
+                //    context.Response.Cookies.Delete("counter1");
+                //    context.Response.Cookies.Delete("counter2");
+                //    context.Response.Redirect("/");
+                //    return Task.CompletedTask;
+                //});
                 endpoints.MapFallback(async context => await context.Response.WriteAsync("Hello world!"));
             });
 
