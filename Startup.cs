eccf6b4b248cb730b0404b417ab6214597aa25c8
Roman Matanin <romashka951@gmail.com>
2022-08-25 13:41:07 +0300

    Кэширование ответов 475

--- Startup.cs
+++ Startup.cs
@@ -3,22 +3,36 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.HostFiltering;
 using System;
 using System.Threading.Tasks;
+using Platform.Services;
 
 namespace Platform
 {
     public class Startup
     {
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private IConfiguration _config { get; set; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDistributedMemoryCache(opts =>
+            ///Кэш хронящийся в БД
+            services.AddDistributedSqlServerCache(opts =>
             {
-                opts.SizeLimit = 200;
+                opts.ConnectionString = _config["ConnectionStrings:CacheConnection"];
+                opts.SchemaName = "dbo";
+                opts.TableName = "DataCache";
             });
+
+            services.AddResponseCaching();
+            services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,6 +46,9 @@ namespace Platform
             {
                 app.UseHsts();
             }
+
+            app.UseResponseCaching();
+
             app.UseStaticFiles();
 
             app.UseRouting();
